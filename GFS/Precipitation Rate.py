05e49605a0b39384e18789759856c03d2e6a1e8b
CustomWX <59298824+CustomWXgraphics@users.noreply.github.com>
2021-02-23 10:40:23 -0500

    Update Precipitation Rate.py

--- GFS/Precipitation Rate.py
+++ GFS/Precipitation Rate.py
@@ -40,18 +40,33 @@ elif hour in range(13,19):
 elif hour in range(19,24):
     run_hour = "1800"
 
+    
 #Set dataset name
-dataset = f"https://thredds-test.unidata.ucar.edu/thredds/dodsC/grib/NCEP/GFS/Global_0p25deg/GFS_Global_0p25deg_{run_date}_{run_hour}.grib2"
+data = xr.open_dataset("https://thredds-jumbo.unidata.ucar.edu/thredds/dodsC/grib/NCEP/GFS/Global_0p25deg/Best")
 
-lat_north = 55
+hour = 360
+
+if hour <= 240:
+    hour /= 3
+    if hour % 3 != int(hour % 3):
+        print(hour % 3)
+        print("Invalid Hour: Rounding to the closest time")
+        hour = round(hour, 0)
+else:
+    hour -= 240
+    total = hour/12
+    if hour % 12 != int(hour % 12):
+        print("Invalid Hour: Rounding to the closest time")
+        round(hour, 0)
+    hour = 80 + total
+hour = int(hour) -1
+
+lat_north = 59
 lat_south = -10
 lon_west = 360
 lon_east = 250
 data = xr.open_dataset(dataset)
 data = data.sel(lat=slice(lat_north,lat_south), lon=slice(lon_east,lon_west))
-
-#Select hour
-hour = 40
 
 lon = data["lon"]
 lat = data["lat"]
@@ -59,27 +74,35 @@ rate = data["Precipitation_rate_surface_Mixed_intervals_Average"][hour] * 900
 msl_hp = data["Pressure_reduced_to_MSL_msl"][hour] / 100
 
 #Plot time
-time1 = datetime.strptime(str(data.time.data[hour].astype('datetime64[ms]')),
+time1 = datetime.strptime(str(data.time.data[80].astype('datetime64[ms]')),
                           '%Y-%m-%dT%H:%M:%S.%f')
 time2 = datetime.strftime(time1, "%Y-%m-%d %H00 UCT")
 
+print("----> Read and defined data")
+
 #Create figure
-fig = plt.figure(figsize=(14,9))
-ax = fig.add_axes([1,1,1,1],projection=ccrs.Miller())
+#Create figure
+fig = plt.figure(figsize=(18,12))
+ax = plt.subplot(1,1,1, projection=ccrs.PlateCarree()) #main plot
+
+proj_ccrs = ccrs.PlateCarree()
 
 #List Coordinates
 Lat_west = -105
-Lat_east =-55
-Lon_south = 15
-Lon_north = 38
+Lat_east =-0
+Lon_south = 0
+Lon_north = 45
 
 ax.set_extent([Lat_east, Lat_west, Lon_north, Lon_south])
+
 
 #add landforms
 ax.add_feature(cfeat.LAKES.with_scale('50m'), facecolor='white')
 ax.add_feature(cfeat.STATES.with_scale('50m'), linewidth=0.4, zorder=7)
 ax.add_feature(cfeat.COASTLINE.with_scale('50m'), edgecolor='#4F5457', zorder=6)
 ax.add_feature(cfeat.BORDERS.with_scale('50m'), edgecolor='#4F5457', zorder=8)
+
+print("----> Set up cartopy projection & geography")
 
 #define times
 if run_hour == "0600" or "1800":
@@ -90,14 +113,73 @@ else:
     Init1 = datetime.strptime(str(data.time.data[0].astype('datetime64[ms]')),
                             '%Y-%m-%dT%H:%M:%S.%f') 
     Init = datetime.strftime(Init1, "%Y-%m-%d %Hz")
-    
-Valid1 = datetime.strptime(str(data.time1.data[hour].astype('datetime64[ms]')),
-                          '%Y-%m-%dT%H:%M:%S.%f')
+
+Valid1 = datetime.strptime(str(data.time.data[hour].astype('datetime64[ms]')),
+                          '%Y-%m-%dT%H:%M:%S.%f') - timedelta(hours=6)
 Valid = datetime.strftime(Valid1, "%Y-%m-%d %Hz")
 
+#Label MSLP extrema
+def extrema(mat,mode='wrap',window=75):
+        mn = minimum_filter(mat, size=window, mode=mode)
+        mx = maximum_filter(mat, size=window, mode=mode)
+        return np.nonzero(mat == mn), np.nonzero(mat == mx)
+
+def mslp_label(mslp,lat,lon):
+    
+    #Determine an appropriate window given the lat/lon grid resolution
+    mslp = np.ma.masked_invalid(mslp)
+    local_min, local_max = extrema(mslp, mode='wrap', window=50)
+    
+    #Determine axis boundaries
+    xmin, xmax, ymin, ymax = ax.get_extent()
+    lons2d, lats2d = np.meshgrid(lon, lat)
+    transformed = proj_ccrs.transform_points(ccrs.PlateCarree(), lons2d, lats2d)
+    x = transformed[..., 0]
+    y = transformed[..., 1]
+    
+    #Get location of extrema on grid
+    xlows = x[local_min]; xhighs = x[local_max]
+    ylows = y[local_min]; yhighs = y[local_max]
+    lowvals = mslp[local_min]; highvals = mslp[local_max]
+    yoffset = 0.022*(ymax-ymin)
+    dmin = yoffset
+    
+    #Plot low pressures
+    xyplotted = []
+    for x,y,p in zip(xlows, ylows, lowvals):
+        if x < xmax-yoffset and x > xmin+yoffset and y < ymax-yoffset and y > ymin+yoffset:
+            dist = [np.sqrt((x-x0)**2+(y-y0)**2) for x0,y0 in xyplotted]
+            if not dist or min(dist) > dmin: #,fontweight='bold'
+                a = ax.text(x,y,'L',fontsize=20, 
+                        ha='center',va='center',color='r',fontweight='normal')
+                b = ax.text(x,y-yoffset,repr(int(p)),fontsize=12, 
+                        ha='center',va='top',color='r',fontweight='normal')
+                a.set_path_effects([path_effects.Stroke(linewidth=1.5, foreground='black'),
+                       path_effects.SimpleLineShadow(),path_effects.Normal()])
+                b.set_path_effects([path_effects.Stroke(linewidth=1.5, foreground='black'),
+                       path_effects.SimpleLineShadow(),path_effects.Normal()])
+                xyplotted.append((x,y))
+                
+    #Plot high pressures
+    xyplotted = []
+    for x,y,p in zip(xhighs, yhighs, highvals):
+        if x < xmax-yoffset and x > xmin+yoffset and y < ymax-yoffset and y > ymin+yoffset:
+            dist = [np.sqrt((x-x0)**2+(y-y0)**2) for x0,y0 in xyplotted]
+            if not dist or min(dist) > dmin:
+                a = ax.text(x,y,'H',fontsize=20, 
+                        ha='center',va='center',color='b',fontweight='normal')
+                b = ax.text(x,y-yoffset,repr(int(p)),fontsize=12,  
+                        ha='center',va='top',color='b',fontweight='normal')
+                a.set_path_effects([path_effects.Stroke(linewidth=1.5, foreground='black'),
+                       path_effects.SimpleLineShadow(),path_effects.Normal()])
+                b.set_path_effects([path_effects.Stroke(linewidth=1.5, foreground='black'),
+                       path_effects.SimpleLineShadow(),path_effects.Normal()])
+                xyplotted.append((x,y))
+
+
 #Find Forecast hour
-data = pd.date_range(start=Init1, end=Valid1, freq='60min')
-hours = len(data) -1 + 6
+data3 = pd.date_range(start=Init1, end=Valid1, freq='60min')
+hours = len(data3) -1 + 6
 
     
 clevs = [0.01,0.03,0.05,0.07,0.1,0.15,0.2,0.25,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,
@@ -116,15 +198,23 @@ norm = col.BoundaryNorm(clevs,cmap.N)
 
 rate = ax.contourf(lon,lat,rate, clevs, transform=ccrs.PlateCarree(), norm=norm, cmap=cmap)
 
-ax.contour(lon,lat, msl_hp, 20, transform=ccrs.PlateCarree(), colors="black")
+ax.contour(lon,lat, msl_hp, 20, transform=ccrs.PlateCarree(), colors="black", linewidths=0.7)
+
+mslp_label(msl_hp,lat,lon)
 
 cbar = plt.colorbar(rate, shrink=.74,pad=0.01, aspect=30, ticks=clevs)
 
+print("----> Plotted Preciptation")
+
 #Add watermark
-plt.text(0.99,0.02,'Graphics By: @CustomWX',
+plt.text(0.99,0.02,'TropicalBlog',
         ha='right',va='bottom',transform=ax.transAxes,fontsize=12,color='black',fontweight='bold'
             , bbox={'facecolor':'white', 'alpha':1, 'boxstyle':'square'}, zorder=15)
 
 #Plot Title
 fig.suptitle(r"0.25$^o$ GFS | Precipitation rate [in] | MSLP [hPa] ", transform=ax.transAxes, fontweight="bold", fontsize=15, y=1.084, x=0, ha="left", va="top")
 plt.title(f"Init: {Init} | Valid: {Valid} | Hour: [{hours}]",x=0.0, y=1.017, fontsize=15, loc="left", transform=ax.transAxes)
+
+plt.savefig("graphic.png", bbox_inches="tight")
+
+print("----> Finished!")
