ba2fe0881519ef2fbd3592e6acb874730f123c07
CustomWX <59298824+CustomWXgraphics@users.noreply.github.com>
2021-04-05 12:46:31 -0400

    Update Ptype & Rate.py

--- GFS/Ptype & Rate.py
+++ GFS/Ptype & Rate.py
@@ -11,7 +11,6 @@ from scipy.ndimage.filters import maximum_filter, minimum_filter
 import scipy.ndimage as ndimage
 import datetime as dt
 import pandas as pd
-import geopandas
 
 from metpy.units import units
 import metpy.calc as mpcalc
@@ -97,157 +96,21 @@ remain = Geo_500 - Geo_1000
 
 print("----> Read and defined data")
 
-#Blank Dictionary to be used later
-dict = {}
+#plot figure
+fig = plt.figure(figsize=(18,12))
+ax = plt.subplot(1,1,1, projection=ccrs.LambertConformal()) #main plot
+proj = ccrs.LambertConformal()
 
-#If True, only counties with be plotted with selected state
-Select_State_County = True
+transform = ccrs.PlateCarree()._as_mpl_transform(ax)
 
-#Select State to Zoom to
-try:
-    State = input("Input State Initials or US (Example MA): ")
-    State1 = int(State)
-    print("Invalid Response Defaulting to US ")
-    State = "US"
-except:
-    pass
-
-if State == "US":
-    #plot figure
-    fig = plt.figure(figsize=(18,12))
-    ax = plt.subplot(1,1,1, projection=ccrs.LambertConformal()) #main plot
-    proj = ccrs.LambertConformal()
-
-    transform = ccrs.PlateCarree()._as_mpl_transform(ax)
-
-    #add landforms
-    ax.add_feature(cfeat.STATES.with_scale('50m'), linewidth=0.6, edgecolor='#404040', zorder=2)
-    ax.add_feature(cfeat.COASTLINE.with_scale('50m'), edgecolor='#172520', linewidth=1.2)
-    Lat_west = -123
-    Lat_east = -67
-    Lon_south = 21
-    Lon_north = 50
-    ax.set_extent([Lat_east, Lat_west, Lon_north, Lon_south])
-else:
-    #Select Distance from states (0=close, 100=far)
-
-    try:
-        distance = input("Please Enter Zoom Distance (0 = Close, 50 = Far): ")
-        distance = int(distance)
-    except:
-        print("Invalid Response Defaulting to 25")
-        distance = 25
-
-
-    #Open County and States shapefiles from directory [THESE ARE IN THE GITHUB DIRECTORY]
-    states_shapefile = geopandas.read_file("cb_2018_us_state_5m.shp")
-    counties_shapefile = geopandas.read_file("cb_2018_us_county_5m.shp")
-
-
-    #Associate State (key) w/ Abbreviation (value) ("North Caroline" = "NC") into dictionary
-    dict = pd.Series(states_shapefile["STATEFP"].values, index=states_shapefile["STUSPS"]).to_dict()
-
-    #If the State you selected is a correct State, assign it to variable (chosen_state)
-    for key, value in dict.items():
-        if key == State:
-            chosen_state = value
-
-    #Only plot the counties within your chosen state
-    if Select_State_County == True:
-        county_geometries = counties_shapefile[counties_shapefile['STATEFP'] == str(chosen_state)]
-
-    #Selected only the state we can center
-    selected_state_geometries = states_shapefile[states_shapefile['STATEFP'] == str(chosen_state)]
-
-    #Find Maximum extent value 
-    south = selected_state_geometries.bounds.miny
-    north = selected_state_geometries.bounds.maxy
-    west = selected_state_geometries.bounds.minx
-    east =  selected_state_geometries.bounds.maxx
-
-    #Center longitude and Latitude by finding the average
-    lon1 = float(east)+float(west)
-    lat1 = float(north)+float(south)
-
-    #Define the global projection
-    proj = ccrs.Orthographic(
-        central_longitude = (lon1/2),
-        central_latitude =  (lat1/2)
-    )
-
-    #Plot blank figure
-    fig = plt.figure(figsize=(18,12))
-    ax = fig.add_subplot(1,1,1, projection=proj)
-
-    #Define the distance between North and South
-    ne = float(north-south)
-    we = float(east-west)
-
-    bvalue = []
-    avalue = []
-
-    #Select x, y box ratio
-    x_ratio = 7.45
-    y_ratio = 14
-
-    #Loop through all valid 
-    if float(lat1/2) > 39:
-        for i in range(1,200):
-            a = x_ratio*(i/100)
-            b = y_ratio*(i/100)
-            if b > we and a > ne:
-                avalue.append(a)
-                bvalue.append(b)
-
-        b = bvalue[0+distance]
-        a = avalue[0+distance]
-
-        ne = (a - ne)/2
-        we = (b - we)/2
-
-    else:
-        for i in range(1,200):
-            a = 7.45*(i/100)
-            b = 13.333*(i/100)
-            if b > we and a > ne:
-                avalue.append(a)
-                bvalue.append(b)
-
-        b = bvalue[0+distance]
-        a = avalue[0+distance]
-
-        ne = (a - ne)/2
-        we = (b - we)/2
-
-
-    south = south - ne
-    north = north + ne
-    east = east + we
-    west = west - we 
-
-
-
-    ax.set_extent([west,east,north,south])
-
-    ax.add_geometries(states_shapefile["geometry"], crs=ccrs.PlateCarree(), linewidth=0.5, zorder=7,facecolor="none", edgecolor="black")
-    if Select_State_County == True:
-        ax.add_geometries(county_geometries["geometry"], crs=ccrs.PlateCarree(), edgecolor="black", facecolor="none", linewidth=0.1,zorder=59)
-    if Select_State_County == False:
-        ax.add_geometries(counties_shapefile["geometry"], crs=ccrs.PlateCarree(), edgecolor="black", facecolor="none", linewidth=0.05,zorder=59)
-
-    aspect = sum(np.abs(ax.get_xlim())) / sum(np.abs(ax.get_ylim()))
-    y = sum(np.abs(ax.get_ylim()))/100000
-    x = sum(np.abs(ax.get_xlim()))/100000
-
-    exa = (a-y)/2
-    exb = (b-x)/2
-
-    south = south
-    north = north
-    east = east + exb - (exa*2)
-    west = west - exb + (exa*2)
-
-    ax.set_extent([west,east,north,south])
+#add landforms
+ax.add_feature(cfeat.STATES.with_scale('50m'), linewidth=0.6, edgecolor='#404040', zorder=2)
+ax.add_feature(cfeat.COASTLINE.with_scale('50m'), edgecolor='#172520', linewidth=1.2)
+Lat_west = -123
+Lat_east = -67
+Lon_south = 21
+Lon_north = 50
+ax.set_extent([Lat_east, Lat_west, Lon_north, Lon_south])
 
 def smooth(prod,sig):
     
